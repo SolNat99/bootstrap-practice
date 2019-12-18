c4c250a3a4e36f4675d5750a1ee0a4a10fffdac1
SolNat99 <natalieas01@gmail.com>
2019-12-18 14:13:51 -0800

    style.css must be OUTSIDE of css folder

---
+++ wordpress/wp-content/themes/boostrap-practice/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <h5>I am the footer</h5>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- wordpress/wp-content/themes/boostrap-practice/index.php
+++ wordpress/wp-content/themes/boostrap-practice/index.php
@@ -1,1 +1,5 @@
+<?php get_header(); ?>
 <h1>This is your theme to practice to BS4</h1>
+
+
+<?php get_footer(); ?>
